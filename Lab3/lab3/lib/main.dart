343768b3267c464d4c8d90125ad8c678cbb99e45
DaryaRybalchenko <rybalchenkodarya2005@gmail.com>
2026-03-25 14:52:58 +0300

    feat: add two buttons with '-' and '+' with the appropriate functions

--- Lab3/lab3/lib/main.dart
+++ Lab3/lab3/lib/main.dart
@@ -33,9 +33,15 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
-  void _incrementCounter() {
+  void incrementCounter() {
     setState(() {
       _counter++;
+    });
+  }
+
+  void decrementCounter() {
+    setState(() {
+      _counter--;
     });
   }
 
@@ -50,18 +56,30 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: .center,
           children: [
-            const Text('You have pushed the button this many times:'),
+            const Text('Значение инкремента:'),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
+            Row(
+              mainAxisAlignment: .center,
+              children: [
+                FloatingActionButton(
+                  onPressed: decrementCounter,
+                  tooltip: 'Decrement',
+                  child: const Icon(Icons.remove),
+                  backgroundColor: const Color.fromARGB(255, 209, 61, 31),
+                ),
+                FloatingActionButton(
+                  onPressed: incrementCounter,
+                  tooltip: 'Increment',
+                  child: const Icon(Icons.add),
+                  backgroundColor: const Color.fromARGB(255, 153, 175, 76),
+                ),
+              ],
+            ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
